4fd8cc2119a232b8966f4ca6257991ee79f96f7d
Xevnar <Xevnar@gmail.com>
2022-10-14 22:18:55 +0300

    fix(config): fix defining 'vsnip' keybindings

--- files/.config/nvim/lua/keybindings/vsnip.lua
+++ files/.config/nvim/lua/keybindings/vsnip.lua
@@ -3,33 +3,22 @@ local t = require('keybindings.utils').t
 local M = {}
 
 function M.setup()
-	-- Define vsnip function wrappers
-	function _G.vsnip_jumpable(direction, jump, default)
-		return vim.fn['vsnip#jumpable'](direction) and jump or default
-	end
+	-- I don't know how to translate this to lua ¯\_(ツ)_/¯
+	vim.cmd([[
+		" Expand
+		imap <expr> <C-j>   vsnip#expandable()  ? '<Plug>(vsnip-expand)'         : '<C-j>'
+		smap <expr> <C-j>   vsnip#expandable()  ? '<Plug>(vsnip-expand)'         : '<C-j>'
 
-	function _G.vsnip_expandable(jump, default)
-		return vim.fn['vsnip#expandable']() and jump or default
-	end
+		" Expand or jump
+		imap <expr> <C-l>   vsnip#available(1)  ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>'
+		smap <expr> <C-l>   vsnip#available(1)  ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>'
 
-	function _G.vsnip_avialable(jump, default)
-		return vim.fn['vsnip#available'](1) and jump or default
-	end
-
-	-- Vsnip  keybinds
-	local keybind = vim.api.nvim_set_keymap
-	local opts = { expr = true, }
-	keybind('i', t'<S-Tab>', t[[v:lua.vsnip_jumpable(-1, '<Plug>(vsnip-jump-prev)', '<S-Tab>')]], opts)
-	keybind('s', t'<S-Tab>', t[[v:lua.vsnip_jumpable(-1, '<Plug>(vsnip-jump-prev)', '<S-Tab>')]], opts)
-
-	keybind('i', t'<Tab>', t[[v:lua.vsnip_jumpable(1, '<Plug>(vsnip-jump-next)', '<Tab>')]], opts)
-	keybind('s', t'<Tab>', t[[v:lua.vsnip_jumpable(1, '<Plug>(vsnip-jump-next)', '<Tab>')]], opts)
-
-	keybind('i', t'<C-j>', t[[v:lua.vsnip_expandable('<Plug>(vsnip-expand)', '<C-j>')]], opts)
-	keybind('s', t'<C-j>', t[[v:lua.vsnip_expandable('<Plug>(vsnip-expand)', '<C-j>')]], opts)
-
-	keybind('i', t'<C-l>', t[[v:lua.vsnip_available('<Plug>(vsnip-expand-or-jump)', '<C-l>')]], opts)
-	keybind('s', t'<C-l>', t[[v:lua.vsnip_available('<Plug>(vsnip-expand-or-jump)', '<C-l>')]], opts)
+		" Jump forward or backward
+		imap <expr> <Tab>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<Tab>'
+		smap <expr> <Tab>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<Tab>'
+		imap <expr> <S-Tab> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'
+		smap <expr> <S-Tab> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'
+	]])
 end
 
 return M
