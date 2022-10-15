5cb1e42cea38188c864c299563770bb72a1896cd
Xevnar <Xevnar@gmail.com>
2022-10-16 00:18:00 +0300

    fix(config): stop using deprecated `default_capabilites`

--- files/.config/nvim/lua/plugs/configs/autocmp.lua
+++ files/.config/nvim/lua/plugs/configs/autocmp.lua
@@ -148,7 +148,7 @@ function M.setup(args)
 		})
 	end
 
-	return require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+	return require('cmp_nvim_lsp').default_capabilities()
 end
 
 return M
